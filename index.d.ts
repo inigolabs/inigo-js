a492511fc8dfc5bee87fe149d35d0ccd9b0bbb00
Michael Skorokhodov <m.skorokhodov@icloud.com>
2022-07-21 14:17:18 -0700

    ts definition file
    commit-id:bcfd8194

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,36 @@
-declare module "inigo.js";
+import type { Type } from "ref-napi";
+import type { StructType } from "ref-struct-di";
+
+export const InigoConfig: StructType<{
+  Debug: Type<boolean>;
+  Ingest: Type<string>;
+  Service: Type<string>;
+  Token: Type<string>;
+  Schema: Type<string>;
+  Introspection: Type<string>;
+}>;
+
+export function InigoPlugin(config: ReturnType<typeof InigoConfig>): any;
+
+interface InigoError {
+  message: string;
+}
+
+export interface InigoAuthContext extends Record<string, any> {
+  readonly req: Record<string, any> & {
+    readonly inigo: {
+      ctx?: any;
+      jwt?: string;
+    }
+  }
+}
+
+export interface InigoProcessedContext extends Record<string, any> {
+  readonly inigo: {
+    readonly blocked: boolean;
+    readonly result: {
+      readonly status: string;
+      readonly errors: InigoError[]
+    };
+  }
+}
