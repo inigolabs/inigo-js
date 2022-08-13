11d422c669ededcf8a77746feb55c775b4fc7205
adam-epam <adam_benkhassen@epam.com>
2022-08-13 16:26:04 +0300

    simplify example
    commit-id:5c3798d1

--- example/src/index.ts
+++ example/src/index.ts
@@ -5,23 +5,18 @@ import { resolve } from "path";
 import { parse } from "yaml";
 import { InigoPlugin, InigoConfig } from 'inigo.js';
 import * as url from "url";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import resolvers from "./resolvers.js"
 
 config();
-const { 
-  INIGO_SCHEMA_PATH, 
-  INIGO_DATA_PATH, 
-  INIGO_PORT, 
-  INIGO_SERVICE_TOKEN,
-} = process.env;
+const { INIGO_SERVICE_TOKEN } = process.env;
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-const typeDefs = readFileSync(resolve(__dirname, "..", "data", INIGO_SCHEMA_PATH), "utf-8");
-const data = parse(readFileSync(resolve(__dirname, "..", "data", INIGO_DATA_PATH), "utf-8"));
+const cwd = resolve(__dirname, "..");
+
+const typeDefs = readFileSync(resolve(cwd, "data/schema.graphql"), "utf-8");
+const data = parse(readFileSync(resolve(cwd, "data/starwars_data.yaml"), "utf-8"));
 
 const inigoCfg = new InigoConfig({
-  Debug: false,
   Token: INIGO_SERVICE_TOKEN,
   Schema: typeDefs
 });
@@ -30,13 +25,11 @@ const server = new ApolloServer({
   debug: false,
   typeDefs,
   resolvers: resolvers(data),
-  introspection: true,
   plugins: [
     InigoPlugin(inigoCfg),
-    ApolloServerPluginLandingPageGraphQLPlayground(),
   ]
 });
 
-server.listen({ port: INIGO_PORT ? Number(INIGO_PORT) : undefined }).then((url) => {
-  console.log(`🚀 Server ready at http://localhost:${url.port}${server.graphqlPath}`);
+server.listen().then((url) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
