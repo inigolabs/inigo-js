85222be0aa2432ec95018ec331f31ba872956b02
Adam Benhassen <adam@inigo.io>
2025-02-10 10:17:04 +0100

    fix library module resolve, update dependency versions
    commit-id:89a2058b

--- ffi.js
+++ ffi.js
@@ -27,11 +27,7 @@ if (getOS() == "darwin") {
     ext = ".dylib"
 }
 
-let libraryPath = resolve(__dirname, `../${pf}/${pf}${ext}`);
-
-if (!fs.existsSync(libraryPath)) {
-  libraryPath = resolve(process.cwd(), "node_modules", `${pf}/${pf}${ext}`);
-}
+let libraryPath = require.resolve(`${pf}/${pf}${ext}`);
 
 if (fs.existsSync(libinigo+ext)) {
   libraryPath = libinigo+ext
